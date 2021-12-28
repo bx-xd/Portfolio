3bd6a36cab3e5dda87bccdb19e001f82ce977ba3
Antoine Brique <antoine.brique@gmail.com>
2021-12-28 11:38:26 +0100

    try to transition for exit projects div display

--- main.js
+++ main.js
@@ -1,20 +1,28 @@
 const anchor = document.getElementById('projects-anchor');
 const projects = document.getElementsByClassName('project-content')[0];
-const main = document.getElementById('main');
+const header = document.getElementById('header');
 const bg = document.getElementById('bg');
+const footer = document.getElementsByTagName('footer')[0];
 
-const toggle = () => {
+const popProjects = () => {
   anchor.addEventListener("click", () => {
-    projects.classList.toggle('active');
-    main.classList.toggle('blured');
+    projects.classList.remove('disactive');
+    projects.classList.add('active');
+    bg.classList.add('blured');
+    header.classList.add('blured');
+    footer.classList.add('blured');
   })
 }
 
 const removeActive = () => {
   bg.addEventListener("click", () => {
-    projects.classList.remove('active')
+    projects.classList.remove('active');
+    projects.classList.add('disactive');
+    bg.classList.remove('blured');
+    header.classList.remove('blured');
+    footer.classList.remove('blured');
   })
 }
 
-toggle();
+popProjects();
 removeActive();
