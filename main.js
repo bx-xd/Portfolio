631736be74fb2773713c0da1b28c31907c70167c
Antoine Brique <antoine.brique@gmail.com>
2021-12-28 10:33:14 +0100

    add wtmany project, add pictures, add event to remove class active when click on bg

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
-const toggle = () => {
-  const anchor = document.getElementById('projects-anchor');
-  const projects = document.getElementsByClassName('project-content')[0];
-  const main = document.getElementById('main');
+const anchor = document.getElementById('projects-anchor');
+const projects = document.getElementsByClassName('project-content')[0];
+const main = document.getElementById('main');
+const bg = document.getElementById('bg');
 
+const toggle = () => {
   anchor.addEventListener("click", () => {
     projects.classList.toggle('active');
     main.classList.toggle('blured');
   })
 }
 
+const removeActive = () => {
+  bg.addEventListener("click", () => {
+    projects.classList.remove('active')
+  })
+}
+
 toggle();
+removeActive();
